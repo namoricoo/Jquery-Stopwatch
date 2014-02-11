7a77c054f1d6dd99ed688e8ce6d0c81b9f676310
Nate <namoricoo@hotmail.com>
2014-02-10 21:35:07 -0800

    first commit

---
+++ spec/jquery.stopwatchSpec.js
@@ -0,0 +1,14 @@
+describe("Stopwatch", function() {
+	var stopwatch;
+	beforeEach(function() {
+		stopwatch = new JqueryStopwatch();
+	});
+	
+	describe('count up stopwatch', function() {
+		it('should have prototype Stopwatch defined', function () {
+			expect(Stopwatch).toBeDefined();
+		});
+
+	
+	});
+});
